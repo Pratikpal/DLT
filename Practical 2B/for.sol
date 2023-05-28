aeacb5f08f55d0732399c967d1c29ed36085b7da
Pratikpal <92366451+Pratikpal@users.noreply.github.com>
2023-05-28 13:30:44 +0530

    Create for.sol

---
+++ Practical 2B/for.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+contract DemoContract{
+    uint storedData;
+    function set(uint x) public{
+        storedData = x;
+        uint i;
+        for (i=0; i !=5; i++){
+            storedData = storedData + i;
+        }
+    function get() public view returns (uint){
+        return storedData;
+    }
+    
+}
